ef9800a04f3f48b5d30615d9b0352f95fdf112c4
SpaceKitty <spacekitty656@gmail.com>
2026-02-16 12:10:15 -0700

    Add unified unit and e2e test tooling setup

---
+++ playwright.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests/e2e',
+  use: {
+    baseURL: 'http://127.0.0.1:4173'
+  },
+  webServer: {
+    command: 'npx serve . -l 4173',
+    url: 'http://127.0.0.1:4173',
+    reuseExistingServer: true,
+    timeout: 120000
+  }
+});
